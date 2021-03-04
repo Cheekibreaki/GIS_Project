a1a02af6f79c68851956b32bba1243ed8f4727b3
Yang Chen <jackchen.chen@mail.utoronto.ca>
2021-03-03 19:40:21 -0500

    m2.cpp renamed to drawMap.cpp

--- libstreetmap/src/drawMap.cpp
+++ libstreetmap/src/drawMap.cpp
@@ -1,10 +1,12 @@
 //
 // Created by cheny811 on 2021-03-03.
 //
-
 #include "m2.h"
 #include "ezgl/application.hpp"
 #include "ezgl/graphics.hpp"
+#include "m1.cpp"
+
+void draw_main_canvas (ezgl::renderer *g);
 
 void drawMap(){
     ezgl::application::settings settings;
@@ -14,8 +16,17 @@ void drawMap(){
 
     ezgl::application application(settings);
 
+    ezgl::rectangle initial_world{{0,0},{1000,1000}};
+
+    application.add_canvas("MainCanvas", draw_main_canvas, initial_world);
+
 
 
     application.run(nullptr, nullptr,
                     nullptr, nullptr);
+}
+
+void draw_main_canvas(ezgl::renderer *g){
+    g->draw_rectangle({0, 0},{1000, 1000});
+
 }
