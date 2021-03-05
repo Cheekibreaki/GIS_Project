296044759be8364d56b24e7eb0d116eb3b4b111f
Jiping <jiping.li@mail.utoronto.ca>
2021-03-04 23:07:41 -0500

    Finished:Street Segments drawing and legends

--- libstreetmap/src/drawMap.cpp
+++ libstreetmap/src/drawMap.cpp
@@ -12,16 +12,22 @@
 #include "m2.h"
 
 
+
 double avg_lat = 0;
 std::vector<intersect_info> IntersectInfoList;
-
 void draw_main_canvas (ezgl::renderer *g);
 void draw_intersection(ezgl::renderer *g);
 void draw_streetSeg(ezgl::renderer *g);
 void draw_naturalFeature(ezgl::renderer *g);
+void draw_legend(ezgl::renderer *g);
 void act_on_mouse_click(ezgl::application* app, GdkEventButton* event, double x, double y);
 
-
+position_XY LatLon_to_posXY(LatLon curLatLon){
+    position_XY tempPos;
+    tempPos.x = x_from_lon(curLatLon.longitude());
+    tempPos.y = y_from_lat(curLatLon.latitude());
+    return tempPos;
+}
 
 void drawMap(){
 
@@ -45,8 +51,7 @@ void drawMap(){
     IntersectInfoList.resize(getNumIntersections());
 
     for(IntersectionIdx id = 0; id < IntersectListOfLatLon.size(); id++){
-        IntersectInfoList[id].curPosXY.x = x_from_lon(IntersectListOfLatLon[id].longitude());
-        IntersectInfoList[id].curPosXY.y = y_from_lat(IntersectListOfLatLon[id].latitude());
+        IntersectInfoList[id].curPosXY = LatLon_to_posXY(IntersectListOfLatLon[id]);
         IntersectInfoList[id].name = getIntersectionName(id);
     }
 
@@ -59,10 +64,12 @@ void drawMap(){
     ezgl::application application(settings);
 
     //set init coordinate system
-    ezgl::rectangle initial_world{{x_from_lon(min_lon),y_from_lat(min_lat)},
+    ezgl::rectangle initial_world = ezgl::rectangle{{x_from_lon(min_lon),y_from_lat(min_lat)},
                                   {x_from_lon(max_lon),y_from_lat(max_lat)}};
 
-    application.add_canvas("MainCanvas", draw_main_canvas, initial_world);
+
+
+    application.add_canvas("MainCanvas", draw_main_canvas, initial_world,ezgl::GREY_55);
 
 
 
@@ -72,14 +79,73 @@ void drawMap(){
 
 void draw_main_canvas(ezgl::renderer *g){
 
-    //draw_intersection(g);
+
+
+    draw_intersection(g);
 
     draw_streetSeg(g);
     draw_naturalFeature(g);
+    draw_legend(g);
 }
+float legendLength;
+void calcLegendLength(ezgl::renderer *g){
+    // Calculate LegendLength
+    // (visibleWorld.right-visibleWorld.left)/(visibleScreen.right-visibleScreen.left) * 100
+    // [m/100 pixel]
+    ezgl::rectangle currentScreen = g->get_visible_screen();
+    ezgl::rectangle currentWorld = g->get_visible_world();
 
-void draw_streetSeg(ezgl::renderer *g){
+    legendLength = 100 * (currentWorld.right()-currentWorld.left())/(currentScreen.right()-currentScreen.left());
+}
+void draw_legend(ezgl::renderer *g){
+    g->set_coordinate_system(ezgl::SCREEN);
 
+    g->set_color(255, 255, 255, 100);
+    g->fill_rectangle({10, 10}, {130, 30});
+
+    g->set_color(0, 0, 0, 255);
+    g->set_line_width(2);
+    g->draw_line({20, 25}, {120, 25});
+    g->draw_line({20, 25}, {20, 20});
+    g->draw_line({120, 25}, {120, 20});
+
+    calcLegendLength(g);
+    std::string legendText = std::to_string(legendLength);
+
+    g->draw_text({70,18},legendText);
+
+    g->set_coordinate_system(ezgl::WORLD);
+}
+void draw_streetSeg(ezgl::renderer *g) {
+
+    for(int segIdx = 0; segIdx<SegListSegInfo.size(); segIdx++){
+
+
+
+        position_XY fromPos=IntersectInfoList[SegListSegInfo[segIdx].from].curPosXY;
+        position_XY toPos=IntersectInfoList[SegListSegInfo[segIdx].to].curPosXY;
+
+        g->set_color(255,255,255,255);
+
+        int numCurvePoints = SegListSegInfo[segIdx].numCurvePoints;
+        if(numCurvePoints != 0){
+            //start of fromPos connect first curvePoint
+            position_XY lastCurvePos = fromPos;
+
+            //for loop through all curvePoint
+            for(int curCurvePointNum=0;curCurvePointNum < numCurvePoints;curCurvePointNum++){
+                position_XY tempCurvePos = LatLon_to_posXY(getStreetSegmentCurvePoint(segIdx,curCurvePointNum));
+
+                g->draw_line({tempCurvePos.x,tempCurvePos.y},{lastCurvePos.x, lastCurvePos.y});
+                lastCurvePos = tempCurvePos;
+            }
+            //draw the last curvePoint to toPos
+            g->draw_line({lastCurvePos.x, lastCurvePos.y},{toPos.x, toPos.y});
+
+        }else{
+            g->draw_line({fromPos.x,fromPos.y},{toPos.x, toPos.y});
+        }
+    }
 }
 void draw_naturalFeature(ezgl::renderer *g){
 
