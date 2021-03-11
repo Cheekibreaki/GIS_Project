b9ce518401ed36a537aad843caef72562ef7d43e
Yang Chen <jackchen.chen@mail.utoronto.ca>
2021-03-11 00:55:55 -0500

    change name

--- libstreetmap/src/m1.cpp
+++ libstreetmap/src/m1.cpp
@@ -442,10 +442,10 @@ bool loadMap(std::string map_streets_database_filename) {
     std::cout << "Loading: IntersectInfoList...." << std::endl;
     LoadIntersectInfoList();
 
-    std::cout << "Loading: IntersectInfoList...." << std::endl;
+    std::cout << "Loading: NaturalFeatureList...." << std::endl;
     LoadNaturalFeatureList();
 
-    std::cout << "Loading: IntersectInfoList...." << std::endl;
+    std::cout << "Loading: NaturalFeatureTypeList...." << std::endl;
     LoadNaturalFeatureTypeList();
 
     std::cout << "Loading: TypeListOfSegsList_Normal...." << std::endl;
