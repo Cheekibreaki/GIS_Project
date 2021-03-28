86e6f4e9d49cb9f098dbf6bea9b909c0739ce119
Jiping <jiping.li@mail.utoronto.ca>
2021-03-27 23:23:50 -0400

    M3 initialization

--- libstreetmap/src/m3.cpp
+++ libstreetmap/src/m3.cpp
@@ -2,6 +2,26 @@
 // Created by cheny811 on 2021-03-27.
 //
 #include "m3.h"
+#include "m1.h"
+#include "DBstruct.h"
+#include <queue>
+
+struct IntersectNaviInfo{
+    StreetSegmentIdx reachingEdge;
+    double bestTime;
+};
+std::vector<IntersectNaviInfo> IntersectNaviInfoList;
+
+struct WaveElem{
+    int curIntersect;
+    int reachingEdge;
+    double travelTime;
+    WaveElem (int curI, int rE, double tT) {
+        curIntersect = curI;
+        reachingEdge = rE;
+        travelTime = tT;
+    }
+};
 
 double computePathTravelTime(const std::vector<StreetSegmentIdx>& path, const double turn_penalty){
     return -1;
