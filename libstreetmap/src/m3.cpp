2fa7d55b7a81e6f8dc58e29c7fa0316871b3119d
Yang Chen <jackchen.chen@mail.utoronto.ca>
2021-03-27 21:03:27 -0400

    m3.cpp init

---
+++ libstreetmap/src/m3.cpp
@@ -0,0 +1,13 @@
+//
+// Created by cheny811 on 2021-03-27.
+//
+#include "m3,h"
+
+double computePathTravelTime(const std::vector<StreetSegmentIdx>& path, const double turn_penalty){
+
+}
+
+std::vector<StreetSegmentIdx> findPathBetweenIntersections(const IntersectionIdx intersect_id_start,
+        const IntersectionIdx intersect_id_destination, const double turn_penalty){
+
+}
