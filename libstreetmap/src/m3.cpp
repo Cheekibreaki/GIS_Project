f7867bf74f9ec8ecd841ee217425359416abf4f3
yinzhiqi <zhiqiang.yin@mail.utoronto.ca>
2021-03-29 20:45:16 -0400

    debug turn panelty

--- libstreetmap/src/m3.cpp
+++ libstreetmap/src/m3.cpp
@@ -26,7 +26,23 @@ struct WaveElem{
     }
 };
 double computePathTravelTime(const std::vector<StreetSegmentIdx>& path, const double turn_penalty){
-    return -1;
+    double totalTurnPenalty = 0;
+    for(int i=0 ; i < path.size()-1 ; i++ ){
+        if(SegsInfoList[path[i]].segInfo.streetID!=SegsInfoList[path[i+1]].segInfo.streetID){
+            totalTurnPenalty+=turn_penalty;
+        }
+    }
+    double totalTraveTime = 0;
+    for(auto curStSegId : path){
+        totalTraveTime += SegsInfoList[curStSegId].time;
+    }
+
+    std::cout << "(Calculated Total Time)" << totalTraveTime + totalTurnPenalty << "= (travel Time)" << totalTraveTime <<
+              "+ (turn Penalty)" << totalTurnPenalty <<std::endl;
+
+    return totalTraveTime + totalTurnPenalty;
+
+
 }
 
 bool NaviInfoHelper(const IntersectionIdx intersect_id_start,
@@ -105,11 +121,12 @@ bool NaviInfoHelper(
                 // Save To INTERSECTIONINFO into the WaveFront
                 // Initalize with WaveElem("curIntersect", "reachingEdge", "travelTime")
 
-                if(currStSegsId != IntersectNaviInfoList[currIntersectId].reachingEdge
+                if(SegsInfoList[currStSegsId].segInfo.streetID != SegsInfoList[IntersectNaviInfoList[currIntersectId].reachingEdge].segInfo.streetID
                     && currIntersectId != intersect_id_start){
                     WaveFront.push(WaveElem(toIntersect, currStSegsId,
                                             IntersectNaviInfoList[currIntersectId].bestTime + curSegInfo.time + turn_penalty));
-                }else{
+                }
+                if(SegsInfoList[currStSegsId].segInfo.streetID == SegsInfoList[IntersectNaviInfoList[currIntersectId].reachingEdge].segInfo.streetID){
                     WaveFront.push(WaveElem(toIntersect, currStSegsId,
                                             IntersectNaviInfoList[currIntersectId].bestTime + curSegInfo.time));
                 }
@@ -127,6 +144,7 @@ std::vector<StreetSegmentIdx> backTracing(const IntersectionIdx intersect_id_sta
     std::deque<StreetSegmentIdx> path;
 
     auto curIntersectId = intersect_id_destination;
+    std::cout << "bestTime"<<IntersectNaviInfoList[curIntersectId].bestTime<<std::endl;
     auto previousEdge = IntersectNaviInfoList[curIntersectId].reachingEdge;
 
     while(previousEdge != -1){
